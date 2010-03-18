86a9fd7ef1e740fdacf2dbe8aa2a71e87600c0c6
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2010-03-18 04:34:29 +0000

    Removed the editable annotator as it's removed from pmr2.app.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/fieldml.pmr2/trunk@4788 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- fieldml/pmr2/annotator.py
+++ fieldml/pmr2/annotator.py
@@ -4,10 +4,9 @@ import zope.component
 from pmr2.app.factory import named_factory
 from pmr2.app.annotation.interfaces import *
 from pmr2.app.annotation.annotator import ExposureFileAnnotatorBase
-from pmr2.app.annotation.annotator import ExposureFileEditableAnnotatorBase
 
 
-class CMGuiViewerAnnotator(ExposureFileEditableAnnotatorBase):
+class CMGuiViewerAnnotator(ExposureFileAnnotatorBase):
     zope.interface.implements(IExposureFileAnnotator)
     title = u'CMGui Viewer'
     label = u'CMGui Viewer'
